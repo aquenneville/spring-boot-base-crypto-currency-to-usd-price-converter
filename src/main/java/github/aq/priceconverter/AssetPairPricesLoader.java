2443ef84dd72e5054f98ef10166c427728f18eb1
aquenneville <alainquenneville@gmail.com>
2018-09-25 21:01:08 +0100

    Add latest code

--- src/main/java/github/aq/priceconverter/AssetPairPricesLoader.java
+++ src/main/java/github/aq/priceconverter/AssetPairPricesLoader.java
@@ -18,20 +18,37 @@ public class AssetPairPricesLoader {
     public AssetPairPricesLoader() {
         assetPairPrices = new ArrayList<>();
     }
-    
-    public List<AssetPairPrice> load() throws IOException {
+
+    public List<AssetPairPrice> load(AssetPair assetPair) throws IOException {
+        switch(assetPair) {
+            case ETHUSD:
+                return load("src/main/resources/BITFINEX-ETHUSD.csv");
+
+            case BTCUSD:
+                return load("src/main/resources/BCHARTS-KRAKENUSD.csv");
+            default:
+                return null;
+        }
+    }
+
+    public List<AssetPairPrice> load(String filename) throws IOException {
         try (
-                Reader reader = Files.newBufferedReader(Paths.get("BITFINEX-ETHUSD.csv"));
+                Reader reader = Files.newBufferedReader(Paths.get(filename));
                 CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                         .withHeader("Date", "High", "Low", "Mid", "Last", "Bid", "Ask", "Volume")
                         .withIgnoreHeaderCase()
                         .withTrim());
             ) {
+                int i = 0;
                 for (CSVRecord csvRecord : csvParser) {
-                    AssetPairPrice app = new AssetPairPrice();
-                    app.setDate(csvRecord.get("Date"));
-                    app.setLast(csvRecord.get("Last"));
-                    assetPairPrices.add(app);
+                    if (i > 0) {
+                        AssetPairPrice app = new AssetPairPrice();
+                        app.setDate(csvRecord.get("Date"));
+                        app.setLast(csvRecord.get("Last"));
+                        assetPairPrices.add(app);
+
+                    }
+                    i ++;
                 }
                 return assetPairPrices;
         }
--- src/test/java/github/aq/priceconverter/TestAssetPairPricesLoader.java
+++ src/test/java/github/aq/priceconverter/TestAssetPairPricesLoader.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.List;
+
 public class TestAssetPairPricesLoader {
 
     @Before
@@ -12,8 +15,29 @@ public class TestAssetPairPricesLoader {
     }
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testLoad() {
+        AssetPairPricesLoader loader = new AssetPairPricesLoader();
+        try {
+            List<AssetPairPrice> list = loader.load("src/test/resources/BITFINEX-ETHUSD.csv");
+            assertTrue(((List) list).size() >0);
+        } catch(IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+    }
+
+    @Test
+    public void testLoadForEthUsd() {
+        AssetPairPricesLoader loader = new AssetPairPricesLoader();
+        try {
+            List<AssetPairPrice> list = loader.load(AssetPair.ETHUSD);
+            assertTrue(((List) list).size() >0);
+            System.out.println("test");
+            AssetPairPrice app = (AssetPairPrice) list.get(0);
+            System.out.println(app.getDate() + " " + app.getLast());
+        } catch(IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+
     }
 
 }
