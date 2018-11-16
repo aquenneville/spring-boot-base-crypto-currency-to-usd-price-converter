161fde97b4e3a0e8df61189e21448c0d1cdc2bbc
aquenneville <alainquenneville@gmail.com>
2018-11-16 22:36:22 +0000

    Add control on the asset pair passed by the user

--- src/main/java/github/aq/priceconverter/controller/PriceConverterController.java
+++ src/main/java/github/aq/priceconverter/controller/PriceConverterController.java
@@ -19,40 +19,44 @@ import github.aq.priceconverter.service.PriceToUsdExchange;
 public class PriceConverterController {
 
 	//Map<AssetPair, HistoricalAssetPairPriceList> historicalPrices;
-	HistoricalAssetPairPriceDictionary dictionary;
+	private HistoricalAssetPairPriceDictionary dictionary;
 	
 	@RequestMapping(path="/altcoin/{assetPairName}/{price}/{amount}/{date}/value", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody Order convertAssetAmountToUsdValue(@PathVariable String assetPairName, @PathVariable BigDecimal price,
             @PathVariable BigDecimal amount, @PathVariable String date) throws IOException {
 	    AssetPairPrice assetPairPrice = null;
         Order order = null;
-        AssetPair assetPair = AssetPair.valueOf(assetPairName.toUpperCase());
-        if (assetPairName.toUpperCase().endsWith("BTC") || assetPairName.toUpperCase().endsWith("ETH")) {
-            if (dictionary == null) {
-                dictionary = new HistoricalAssetPairPriceDictionary();
-            }
-            dictionary.addHistoricalBaseAssetPairPrices(assetPair);
+        if (AssetPair.contains(assetPairName)) {
+			AssetPair assetPair = AssetPair.valueOf(assetPairName.toUpperCase());
+			if (assetPairName.toUpperCase().endsWith("BTC") || assetPairName.toUpperCase().endsWith("ETH")) {
+				if (dictionary == null) {
+					dictionary = new HistoricalAssetPairPriceDictionary();
+				}
+				dictionary.addHistoricalBaseAssetPairPrices(assetPair);
 
-            if (dictionary.hasAssetPairAndPrice(assetPair, date)) {
-                order = new Order();
-                assetPairPrice = new AssetPairPrice();
-                assetPairPrice.setLast(price.toString());
-                assetPairPrice.setAssetPair(assetPair);
-                order.setApp(assetPairPrice);
-                order.setAmount(amount);
-                
-                // in the case the asset pair does not end with USD 
-                if (!assetPairName.toUpperCase().endsWith("USD")) {
-                    Order usdOrder = new PriceToUsdExchange(assetPair).convertPriceToUsd(LocalDate.parse(date), order.getValue());
-                    order.setOrder(usdOrder);
-                }
-    
-            } else {
-                throw new IllegalStateException("The price does not exist for this date");
-            }
-        } else {
-            throw new IllegalStateException("The asset pair needs to end with BTC or start with ETH.");
-        }
+				if (dictionary.hasAssetPairAndPrice(assetPair, date)) {
+					order = new Order();
+					assetPairPrice = new AssetPairPrice();
+					assetPairPrice.setLast(price.toString());
+					assetPairPrice.setAssetPair(assetPair);
+					order.setApp(assetPairPrice);
+					order.setAmount(amount);
+
+					// in the case the asset pair does not end with USD
+					if (!assetPairName.toUpperCase().endsWith("USD")) {
+						Order usdOrder = new PriceToUsdExchange(assetPair).convertPriceToUsd(LocalDate.parse(date), order.getValue());
+						order.setOrder(usdOrder);
+					}
+
+				} else {
+					throw new IllegalStateException("The price does not exist for this date");
+				}
+			} else {
+				throw new IllegalStateException("The asset pair needs to end with BTC or start with ETH.");
+			}
+		} else {
+			throw new IllegalStateException("The asset pair " + assetPairName + " is unknown");
+		}
         return order;
 	}
 	
@@ -60,30 +64,34 @@ public class PriceConverterController {
 	public @ResponseBody Order convertAssetAmountToUsdValue(@PathVariable String assetPairName, @PathVariable BigDecimal amount, @PathVariable String date) throws IOException {
 		AssetPairPrice app = null;
 		Order order = null;
-		AssetPair assetPair = AssetPair.valueOf(assetPairName.toUpperCase());
-		if (assetPairName.toUpperCase().endsWith("BTC") || assetPairName.toUpperCase().startsWith("ETH")) {
-    		if (dictionary == null) {
-    		    dictionary = new HistoricalAssetPairPriceDictionary();
-    		}
+		if (AssetPair.contains(assetPairName)) {
+			AssetPair assetPair = AssetPair.valueOf(assetPairName.toUpperCase());
+			if (assetPairName.toUpperCase().endsWith("BTC") || assetPairName.toUpperCase().startsWith("ETH")) {
+				if (dictionary == null) {
+					dictionary = new HistoricalAssetPairPriceDictionary();
+				}
 
-    		dictionary.addHistoricalBaseAssetPairPrices(assetPair);
+				dictionary.addHistoricalBaseAssetPairPrices(assetPair);
 
-			if (dictionary.hasAssetPairAndPrice(assetPair, date)) {
-    			app = dictionary.getAssetPairPrice(assetPair, date);
-    			order = new Order();
-    			order.setApp(app);
-    			order.setAmount(amount);
-    			// in the case the asset pair does not end with USD 
-    			if (!assetPairName.toUpperCase().endsWith("USD")) {
-    			    Order usdOrder = new PriceToUsdExchange(assetPair).convertPriceToUsd(LocalDate.parse(date), order.getValue());
-    			    order.setOrder(usdOrder);
-    			}
-    
-    		} else {
-    			throw new IllegalStateException("The price does not exist for this date");
-    		}
+				if (dictionary.hasAssetPairAndPrice(assetPair, date)) {
+					app = dictionary.getAssetPairPrice(assetPair, date);
+					order = new Order();
+					order.setApp(app);
+					order.setAmount(amount);
+					// in the case the asset pair does not end with USD
+					if (!assetPairName.toUpperCase().endsWith("USD")) {
+						Order usdOrder = new PriceToUsdExchange(assetPair).convertPriceToUsd(LocalDate.parse(date), order.getValue());
+						order.setOrder(usdOrder);
+					}
+
+				} else {
+					throw new IllegalStateException("The price does not exist for this date");
+				}
+			} else {
+				throw new IllegalStateException("The asset pair needs to end with BTC or start with ETH.");
+			}
 		} else {
-		    throw new IllegalStateException("The asset pair needs to end with BTC or start with ETH.");
+			throw new IllegalStateException("The asset pair " + assetPairName + " is unknown");
 		}
 		return order;
 	}
--- src/main/java/github/aq/priceconverter/model/AssetPair.java
+++ src/main/java/github/aq/priceconverter/model/AssetPair.java
@@ -1,5 +1,13 @@
 package github.aq.priceconverter.model;
 
+import java.util.Arrays;
+
 public enum AssetPair {
     ETHUSD, ETHEUR, ETHBTC, BTCUSD, EURUSD, XVGETH, TRONETH, NEOETH, ETCETH;
+
+    public static boolean contains(String test) {
+
+        return Arrays.stream(AssetPair.values()).anyMatch((t) -> t.name().equals(test));
+
+    }
 }
